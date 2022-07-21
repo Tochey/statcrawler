ac253006fc8d56883a02350022c2b0b21601c0af
DON <don@don.myfiosgateway.com>
2022-07-21 00:49:35 -0400

    minor refractor changes

--- src/components/services/SpServices.js
+++ src/components/services/SpServices.js
@@ -2,28 +2,29 @@ import React, { useEffect, useState } from "react";
 import network from "../logic/network";
 import { format } from "../logic/formatter";
 
-
-function SpServices({url}) {
+function SpServices({ url }) {
   const networkDaemon = new network(url, 10);
   const [incident, setIncident] = useState([]);
   const [summary, setSummary] = useState();
 
   useEffect(() => {
-    networkDaemon.pollData((e) => {
-      e.map((e) => {
-        setIncident(incident => [...incident, format(e)])
-      })
-    }, (e) => {
-     setSummary(e.description)
-    })
+    networkDaemon.pollData(
+      (e) => {
+        e.map((e) => {
+          setIncident((incident) => [...incident, format(e)]);
+        });
+      },
+      (e) => {
+        setSummary(e.description);
+      }
+    );
     // return () => networkDaemon.clear()
   }, []);
 
-
   return (
     <div>
-    <h1>{summary}</h1>
-      {incident.map((e) =>
+      <h1>{summary}</h1>
+      {incident.map((e) => (
         <div>
           <h1>{e.name}</h1>
           <p>{e.status}</p>
@@ -36,10 +37,10 @@ function SpServices({url}) {
                 <p>{e.status}</p>
                 <p>{e.last_updated}</p>
               </div>
-            )
+            );
           })}
         </div>
-      )}
+      ))}
     </div>
   );
 }
