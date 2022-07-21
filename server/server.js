7f2c3189e4cdceefd7eb82685619bee5ecb9de23
DON <don@DON-2.local>
2022-07-21 14:59:03 -0400

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const app = express()
+
+const SERVER_PORT = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hi')
+    res.end()
+})
+
+app.listen(SERVER_PORT, () =>{
+    console.log(`server si listening on port ${SERVER_PORT}`)
+})
