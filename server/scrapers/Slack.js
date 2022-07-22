cb27f774ad05ccdeb5de64cce39525ebe8104a40
DON <don@DON-2.local>
2022-07-22 11:02:33 -0400

    added cheerio for scraping

---
+++ server/scrapers/Slack.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const pretty = require('pretty')
+
+
+const scrapeSlack  = async () => {
+   const page =  await axios.get("https://status.slack.com/")
+    .then(e => e.data)
+
+    const $ = cheerio.load(page)
+
+const icon = $('img', '.container')
+const status = $('h1', '.container').text()
+    // const scrapeRss = async () => {
+    //     const feed = await axios.get('https://status.slack.com/feed/rss')
+    //         .then(e => e.data)
+
+    //         const $ = cheerio.load(feed, {
+    //             xmlMode: true
+    //         })
+
+    //         const arr = $('item')
+    //         console.log('HERE')
+    //         console.log(arr)
+    // }
+
+    // scrapeRss()
+
+    return {
+        status : status,
+        image : icon
+    }
+}
+
+
+module.exports = scrapeSlack
+
+
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const NetworkDaemon = require('./NetworkDaemon')
-const spformat = require('./formatters/spFormatter')
-const officeformat = require('./formatters/officeFormatter')
+const cheerio = require('cheerio')
+const { default: axios } = require('axios')
 const app = express()
+const scrapeSlack = require('./scrapers/Slack')
 
 const SERVER_PORT = 8080
 let nw = new NetworkDaemon(200)
@@ -14,6 +15,10 @@ app.listen(SERVER_PORT, () =>{
     console.log(`server is listening on port ${SERVER_PORT}`)
 })
 
+
+scrapeSlack().then(e => {
+    console.log(e)
+})
 
 
 
