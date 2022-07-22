fda3af54ded6a84e336c80f32ba4940c5a99841f
DON <don@DON-2.local>
2022-07-22 15:37:25 -0400

    minor changes

--- server/scrapers/Slack.js
+++ server/scrapers/Slack.js
@@ -1,39 +1,48 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const pretty = require('pretty')
+const axios = require("axios");
+const cheerio = require("cheerio");
+const pretty = require("pretty");
 
+const scrapeSlack = async () => {
+  const page = await axios.get("https://status.slack.com/").then((e) => e.data);
 
-const scrapeSlack  = async () => {
-   const page =  await axios.get("https://status.slack.com/")
-    .then(e => e.data)
+  const $ = cheerio.load(page);
 
-    const $ = cheerio.load(page)
+  const icon = $("img", ".container").attr("src");
+  const status = $("h1", ".container").text();
 
-const icon = $('img', '.container')
-const status = $('h1', '.container').text()
+  const scrapeRss = async () => {
+    const feed = await axios
+      .get("https://status.slack.com/feed/rss")
+      .then((e) => e.data);
 
-    // const scrapeRss = async () => {
-    //     const feed = await axios.get('https://status.slack.com/feed/rss')
-    //         .then(e => e.data)
+    const $ = cheerio.load(feed, {
+      xmlMode: true,
+    });
 
-    //         const $ = cheerio.load(feed, {
-    //             xmlMode: true
-    //         })
+    let data = [];
 
-    //         const arr = $('item')
-    //         console.log('HERE')
-    //         console.log(arr)
-    // }
+    console.log($);
 
-    // scrapeRss()
-
-    return {
-        status : status,
-        image : icon
-    }
+    $("item").map((e, text) => {
+    
+      data.push(
+          {
+          title: $(text).find('title')[0].children[0].data,
+          description: $(text).find('description')[0].children[0].data.toString().toLowerCase(),
+          publishdate : $(text).find('pubDate')[0].children[0].data,
+          link : $(text).find('link')[0].children[0].data,
+          }
+      )
+    });
+    console.log(data);
 }
 
+    scrapeRss();
 
-module.exports = scrapeSlack
+    return {
+      status: status,
+      image: icon,
+    };
+  };
 
-
+module.exports = scrapeSlack;
