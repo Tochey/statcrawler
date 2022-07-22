bfb8eccfcaa9ba052d0a843224b79993a01b5d1d
David Ogar <owogogahhero@outlook.com>
2022-07-22 15:52:23 +0100

    refactor: made little change

--- src/components/services/SpServices.js
+++ src/components/services/SpServices.js
@@ -1,44 +1,40 @@
 import React, { useEffect, useState } from "react";
 import network from "../logic/network";
-import { format } from "../logic/formatter";
+// import { format } from "../logic/formatter";
 
 function SpServices({ url }) {
-  const networkDaemon = new network(url, 10);
   const [incident, setIncident] = useState([]);
   const [summary, setSummary] = useState();
 
   useEffect(() => {
+    const networkDaemon = new network(url, 10);
     networkDaemon.pollData(
       (e) => {
-        e.map((e) => {
-          setIncident((incident) => [...incident, format(e)]);
-        });
+        setIncident(e);
       },
       (e) => {
         setSummary(e.description);
       }
     );
     // return () => networkDaemon.clear()
-  }, []);
+  }, [url]);
 
   return (
     <div>
       <h1>{summary}</h1>
-      {incident.map((e) => (
-        <div>
+      {incident.map((e, idx) => (
+        <div key={idx}>
           <h1>{e.name}</h1>
           <p>{e.status}</p>
           <p>{e.date}</p>
           <p>{e.link}</p>
-          {e.incident_history_update.map((e) => {
-            return (
-              <div>
-                <p>{e.body}</p>
-                <p>{e.status}</p>
-                <p>{e.last_updated}</p>
-              </div>
-            );
-          })}
+          {e.incident_updates.map((e, index) => (
+            <div key={`${idx}-${index}`}>
+              <p>{e.body}</p>
+              <p>{e.status}</p>
+              <p>{e.last_updated}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
