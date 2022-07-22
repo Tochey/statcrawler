1cf9a3a2ecf1e01ea961ebc5d83fd847e4362ca5
DON <don@DON-2.local>
2022-07-22 16:13:07 -0400

    added support for slack

--- server/scrapers/Slack.js
+++ server/scrapers/Slack.js
@@ -3,46 +3,51 @@ const cheerio = require("cheerio");
 const pretty = require("pretty");
 
 const scrapeSlack = async () => {
-  const page = await axios.get("https://status.slack.com/").then((e) => e.data);
+    const page = await axios.get("https://status.slack.com/").then((e) => e.data);
 
-  const $ = cheerio.load(page);
+    const $ = cheerio.load(page);
+    const icon = $("img", ".container").attr("src");
+    const status = $("h1", ".container").text();
+    const scrapeRss = async () => {
+        const feed = await axios
+            .get("https://status.slack.com/feed/rss")
+            .then((e) => e.data);
 
-  const icon = $("img", ".container").attr("src");
-  const status = $("h1", ".container").text();
+        const $ = cheerio.load(feed.trim(), {
+            xml: {
+                normalizeWhitespace: true,
+            },
+        });
 
-  const scrapeRss = async () => {
-    const feed = await axios
-      .get("https://status.slack.com/feed/rss")
-      .then((e) => e.data);
+        let data = [];
 
-    const $ = cheerio.load(feed, {
-      xmlMode: true,
-    });
-
-    let data = [];
-
-    console.log($);
-
-    $("item").map((e, text) => {
-    
-      data.push(
-          {
-          title: $(text).find('title')[0].children[0].data,
-          description: $(text).find('description')[0].children[0].data.toString().toLowerCase(),
-          publishdate : $(text).find('pubDate')[0].children[0].data,
-          link : $(text).find('link')[0].children[0].data,
-          }
-      )
-    });
-    console.log(data);
-}
+        $("item").map((e, text) => {
+            data.push({
+                title: $(text).find("title").text().replaceAll("<br />\r\n", ""),
+                description: $(text)
+                    .find("description")
+                    .text()
+                    .replaceAll("<br />\r\n", ""),
+                publishdate: $(text)
+                    .find("pubDate")[0]
+                    .children[0].data.replaceAll("<br />\r\n", ""),
+                link: $(text)
+                    .find("link")[0]
+                    .children[0].data.replaceAll("<br />\r\n", ""),
+            });
+        });
+        return data;
+    };
 
     scrapeRss();
 
     return {
-      status: status,
-      image: icon,
+        status: status,
+        image: icon,
+        incident_history: (await scrapeRss()).map((e) => {
+            return e;
+        }),
     };
-  };
+};
 
 module.exports = scrapeSlack;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,20 @@
-const express = require('express')
-const NetworkDaemon = require('./NetworkDaemon')
-const cheerio = require('cheerio')
-const { default: axios } = require('axios')
-const app = express()
-const scrapeSlack = require('./scrapers/Slack')
+const express = require("express");
+const NetworkDaemon = require("./NetworkDaemon");
+const cheerio = require("cheerio");
+const { default: axios } = require("axios");
+const app = express();
+const scrapeSlack = require("./scrapers/Slack");
 
-const SERVER_PORT = 8080
-let nw = new NetworkDaemon(200)
-app.get('/', (req, res) => {
-        res.end()
-    })
+const SERVER_PORT = 8080;
+let nw = new NetworkDaemon(200);
+app.get("/", (req, res) => {
+    res.end();
+});
 
-app.listen(SERVER_PORT, () =>{
-    console.log(`server is listening on port ${SERVER_PORT}`)
-})
+app.listen(SERVER_PORT, () => {
+    console.log(`server is listening on port ${SERVER_PORT}`);
+});
 
-
-scrapeSlack().then(e => {
-    console.log(e)
-})
-
-
-
-
-
-
+scrapeSlack().then((e) => {
+    console.log(e);
+});
