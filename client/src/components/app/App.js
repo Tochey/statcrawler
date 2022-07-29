c2b5727228416de913de35eacbfa2a97aeea8fee
David Ogar <owogogahhero@outlook.com>
2022-07-29 14:00:26 +0100

    create route

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React  from "react";
 import "./App.css";
 // import SpServices from "../services/SpServices";
 import Navbar from "../layouts/navbar/navbar";
