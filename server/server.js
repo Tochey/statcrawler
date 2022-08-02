78c9cf45ecb97ea893f08950f491a69b2c35cdbc
David Ogar <owogogahhero@outlook.com>
2022-08-02 01:56:32 +0100

    ready to pull

--- server/server.js
+++ server/server.js
@@ -2,22 +2,20 @@ const express = require("express");
 const app = express();
 const serviceRouter = require('./routes/services')
 const cors = require('cors')
-<<<<<<< HEAD
 const dbConnection = require('./config/db.config')
-const userRoute = require('./routes/users')
+const userRoute = require('./routes/users.signup')
 const authRoute = require('./routes/users.login')
 //const mongoose = require('mongoose')
 require('dotenv').config()
 
-const PORT = 8080
-=======
+//const PORT = 8080
+
 const PORT = 8081
->>>>>>> main
+
 
 app.use(cors({
   origin: '*'
 }))
-
 
 //db connection
 dbConnection()
@@ -30,11 +28,10 @@ dbConnection()
 // });
 
 app.use(express.json())
-app.use('/api/v1/user', userRoute)
-app.use('/api/v1/user', authRoute)
+app.use('/api/v1/users', userRoute)
+app.use('/api/v1/users', authRoute)
 
 app.use('/services', serviceRouter)
-
 
 
 app.get("/", async (req, res) => {
