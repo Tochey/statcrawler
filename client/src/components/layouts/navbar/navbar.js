8913bf1c427161e56688458d3e030956644915a9
David Ogar <owogogahhero@outlook.com>
2022-08-09 17:38:43 +0100

    refactor: nav login route

--- client/src/components/layouts/navbar/navbar.js
+++ client/src/components/layouts/navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Login from "../../pages/login";
+//import Login from "../../pages/login";
 
 export default function Navbar() {
   return (
