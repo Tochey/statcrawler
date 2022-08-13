fdbd7d7dcd15e60b6f792a114b6190ca7390d180
David Ogar <owogogahhero@outlook.com>
2022-08-13 14:27:12 +0100

    refactor dashboard response

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/dashboard", auth, async (req, res) => {
-  res.send({message : `Welcome back ${req.user.email}`})
+  res.send({message : `${req.user.email}`})
   res.end()
 });
 
