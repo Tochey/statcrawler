df28bec886e683712d331f9138a6fe90778f3f2a
DON <don@don.myfiosgateway.com>
2022-08-13 23:56:10 -0400

    exposed endpoints to assist with full jwt flow and also implemented the flow client side. cleaned up gitignore files( there were too many ). now storing user refresh tokens in redis

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,15 +4,15 @@ dotenv.config();
 
 //authentication middleware
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["authorization"];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(' ')[1]
 
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
--- server/routes/services.js
+++ server/routes/services.js
@@ -4,42 +4,42 @@ const client = require('../config/redis.config')
 require('../pollServices')
 
 router.get("/zoom", async (req, res) => {
-  client.get("zoomStatus").then((e) => {
+  await client.get("zoomStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
 
 router.get("/notion", async (req, res) => {
-  client.get("notionStatus").then((e) => {
+  await client.get("notionStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
 
 router.get("/slack", async (req, res) => {
-  client.get("slackStatus").then((e) => {
+  await client.get("slackStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
 
 router.get("/egnyte", async (req, res) => {
-  client.get("egnyteStatus").then((e) => {
+  await client.get("egnyteStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
 router.get("/goToAssist", async (req, res) => {
-  client.get("goToAssistStatus").then((e) => {
+  await client.get("goToAssistStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
 
 router.get("/office", async (req, res) => {
-  client.get("officeStatus").then((e) => {
+  await client.get("officeStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
 
 router.get("/jamf", async (req, res) => {
-  client.get("jamfStatus").then((e) => {
+  await client.get("jamfStatus").then((e) => {
     res.json(JSON.parse(e));
   });
 });
