2cdba040fe046a19663f1d4a9e9a69c217ad3809
David Ogar <owogogahhero@outlook.com>
2022-09-18 21:04:16 +0100

    refactor: modal styling button

--- client/src/components/pages/dashboard.cards.js
+++ client/src/components/pages/dashboard.cards.js
@@ -1,12 +1,13 @@
 import React from 'react'
 // import { Link } from 'react-router-dom';
 
-const DashboardCard = ({ logo, status, recentEvent }) => {
+const DashboardCard = ({ logo, status, recentEvent, modal }) => {
   return (
     <div className="h-[350px] mb-4 bg-white w-[330px] cursor-pointer">
       <div className="flex justify-center mb-4 border-b border-black">{logo}</div>
       <div className="flex justify-center mt-3 border h-10 items-center text-white bg-green-600 rounded-md">all systems operational</div>
-      <div className="border-t border-black mt-4"></div>
+      <div className='border-b border-black mt-6'></div>
+      <div className="border-black flex justify-center mt-6">{modal}</div>
       <div>{status}</div>
       <div>{recentEvent}</div>
     </div>
@@ -15,10 +16,6 @@ const DashboardCard = ({ logo, status, recentEvent }) => {
 
 export default DashboardCard;
 /*
-todo: all system operational is supposed to be a component (modal)
-todo: that returns that service status on openModal
-todo: Connect modal to the mapped data in Dashboard.js
-todo: code logic to display colors red/yellow/green depending on service stats
-todo: on the card div, apply something, maybe recent service stats
+todo: code logic to display colors red/yellow/green depends on current service stats
 
 */
