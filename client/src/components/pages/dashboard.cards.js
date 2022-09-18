618e64672682f841390a73576940f168482a7acf
David Ogar <owogogahhero@outlook.com>
2022-09-18 16:31:48 +0100

    refactor: minor

--- client/src/components/pages/dashboard.cards.js
+++ client/src/components/pages/dashboard.cards.js
@@ -4,7 +4,7 @@ import React from 'react'
 const DashboardCard = ({ logo, status, recentEvent }) => {
   return (
     <div className="h-[350px] mb-4 bg-white w-[330px] cursor-pointer">
-      <div className="flex justify-center border-b border-black">{logo}</div>
+      <div className="flex justify-center mb-4 border-b border-black">{logo}</div>
       <div className="flex justify-center mt-3 border h-10 items-center text-white bg-green-600 rounded-md">all systems operational</div>
       <div className="border-t border-black mt-4"></div>
       <div>{status}</div>
@@ -14,3 +14,11 @@ const DashboardCard = ({ logo, status, recentEvent }) => {
 };
 
 export default DashboardCard;
+/*
+todo: all system operational is supposed to be a component (modal)
+todo: that returns that service status on openModal
+todo: Connect modal to the mapped data in Dashboard.js
+todo: code logic to display colors red/yellow/green depending on service stats
+todo: on the card div, apply something, maybe recent service stats
+
+*/
