4977bd52399c68f669aaca4e4983b482643c0319
David Ogar <owogogahhero@outlook.com>
2022-09-22 16:35:03 +0100

    refactor: change header styling

--- client/src/components/pages/modals/NotionModal.js
+++ client/src/components/pages/modals/NotionModal.js
@@ -24,7 +24,7 @@ export default function NotionModal() {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/* modal header */}
                 <div className="flex items-start justify-between p-5 border-b border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Include title here</h3>
+                  <h3 className="text-lg flex justify-center font-semibold">Notion Service Status</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => modalOpen(false)}
