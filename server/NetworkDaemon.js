6613cf2eae52fd80a00fc6613537ffd64b9c5fe1
David Ogar <owogogahhero@outlook.com>
2022-10-05 14:38:28 +0100

    fix pull error

--- server/NetworkDaemon.js
+++ server/NetworkDaemon.js
@@ -75,7 +75,7 @@ class NetworkDaemon {
                 console.error(err); // eslint-disable-line no-console
             }
         }, this.refreshRate * 1000);
-
+        
         this.interval.push(setIntervalId);
     }
 
