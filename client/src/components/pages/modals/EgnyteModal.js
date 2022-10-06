6faec40b2409f48eef44622de17bb28708195566
David Ogar <owogogahhero@outlook.com>
2022-10-06 13:36:32 +0100

    refactor: modal stle

--- client/src/components/pages/modals/EgnyteModal.js
+++ client/src/components/pages/modals/EgnyteModal.js
@@ -17,18 +17,18 @@ export default function EgnyteModal() {
       {modalOpen ? (
         <>
           {/* the modal box */}
-        <div className="w-screen overflow-auto fixed inset-0 z-50 bg-black/50">
-            <div className="w-6/12 bg-white rounded-md my-[80px] mx-auto">
+        <div className="w-screen overflow-auto py-[-30px] fixed inset-0 z-50 bg-black/50">
+            <div className="w-7/12 bg-white rounded-md my-[80px] mx-auto">
              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="p-5 border-b border-slate-200 rounded-t">
-                  <h3 className="text-lg flex justify-center font-semibold">Egnyte Service Status</h3>
+                <div className="mt-6 h-5 flex justify-center items-center border-slate-200 rounded-t">
+                  <h3 className="text-lg font-semibold"> Egnyte Service Status </h3>
                 </div>
-                <div className="w-full text-slate-500 text-lg leading-relaxed">
+                <div className="w-full leading-relaxed">
                   <EgnyteService/>
                 </div> 
-                <div className="flex items-center justify-end p-3 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setModalOpen(false)}
                   >
