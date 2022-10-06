4f5149220910865095c4d5fd7c4e9ef1481866b2
David Ogar <owogogahhero@outlook.com>
2022-10-06 10:26:14 +0100

    refactor: change pagination style

--- client/src/utilities/pagination.js
+++ client/src/utilities/pagination.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
-export default function modalPagination({
+export default function ModalPagination({
     dataPerPage,
     totalData,
     paginate,
@@ -12,15 +12,16 @@ export default function modalPagination({
         modalNumber.push(i);
     }
     return(
-        <div className="py-2 ">
-            <div>
+        <div className="py-2">
+            <div className="flex justify-center mb-2">
                 <p className="text-gray-500 text-sm"> showing 
-                    <span className="font-medium">{""} {currentModalPage * dataPerPage - 10} {""}</span>
+                    <span className="font-medium">{""} {currentModalPage*dataPerPage-10} {""}</span>
                     to
                     <span className="font-medium"> {currentModalPage * dataPerPage} </span>
                 </p>
             </div>
-            <nav className="block">
+            <nav className="block ">
+                <div className="flex justify-center">
                 <ul className="flex pl-0 rounded-full list-none flex-wrap">
                     <li>{modalNumber.map((number) => (
                         <a 
@@ -30,12 +31,13 @@ export default function modalPagination({
                         href="#"
                         className={ 
                             currentModalPage === number
-                            ? "bg-green-500 text-white hover:bg-green-600 relative inline-flex items-center px-4 py-2 text-sm font-medium" :
-                            "bg-white border-gray-400 text-gray-500 hover:bg-green-400 relative inline-flex items-center px-4 py-2 border text-sm font-medium"}>
+                            ? "bg-green-500 rounded-full text-white hover:bg-green-600 relative inline-flex items-center px-4 py-2 text-sm font-medium" :
+                            "border-gray-400 text-white rounded-full bg-green-500 hover:bg-green-400 relative inline-flex items-center px-4 py-2 mr-2 text-sm font-medium"}>
                             {number}
                         </a>
                     ))}</li>
                 </ul>
+                </div>
             </nav>
         </div>
     )
