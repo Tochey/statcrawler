6ea29c9d9123b4c7de702a95ab249cefc77953f1
David Ogar <owogogahhero@outlook.com>
2022-10-06 13:35:15 +0100

    fix: table data

--- client/src/components/pages/services/Egnyte.js
+++ client/src/components/pages/services/Egnyte.js
@@ -1,39 +1,47 @@
-import React, { useState, useEffect} from 'react'
-import ModalPagination from '../../../utilities/pagination';
+import React, { useState, useEffect } from "react";
+import ModalPagination from "../../../utilities/pagination";
 // import { BiFilter } from 'react-icons/bi'
 
-export default function EgnyteService(){
-    //getting the data
-    const [egnyte, setEgnyte] = useState([])
-    const [dataPerPage] = useState(10); // sets the service data per page
-    const [currentModalPage, setCurrentModalPage] = useState(1); // sets the current modal page
-    // const [loading, setLoading] = useState(false);
+export default function EgnyteService() {
+  //getting the data
+  const [egnyte, setEgnyte] = useState([]);
+  const [dataPerPage] = useState(10); // sets the service data per page
+  const [currentModalPage, setCurrentModalPage] = useState(1); // sets the current modal page
 
-    async function getEgnyteServiceData(){
-        const res = await fetch('http://localhost:8081/api/v1/services/egnyte')
-        const req = await res.json()
-        setEgnyte(req.incidents)
+  // const [loading, setLoading] = useState(false);
+  const [paginatedData, setPaginatedData] = useState([]);
+
+  async function getEgnyteServiceData() {
+    try {
+        // console.log('nihere')
+      const res = await fetch("http://localhost:8081/api/v1/services/egnyte");
+      const req = await res.json();
+      setEgnyte(req.incidents);
+      setPaginatedData([...req.incidents.slice(0, dataPerPage)]);
+    } catch (error) {
+      alert(error?.message || error);
     }
-     console.log(egnyte)
-    useEffect(() => {
-        getEgnyteServiceData()
+  }
+  // console.log(egnyte)
+  useEffect(() => {
+    console.log('called');
+    getEgnyteServiceData();
+  }, []);
 
-    }, []);
-
+  // change the modal page
+  const paginate = (modalNumber) => {
     // gets the current service data
-    const indexOfLastData = currentModalPage * dataPerPage;
+    const indexOfLastData = modalNumber * dataPerPage;
     const indexOfFirstData = indexOfLastData - dataPerPage;
-    const currentData = egnyte.slice(indexOfFirstData, indexOfLastData);
+    setPaginatedData([...egnyte.slice(indexOfFirstData, indexOfLastData)])
+    setCurrentModalPage(modalNumber);
+  };
 
-    // change the modal page
-    const paginateModalFront = () => setCurrentModalPage(currentModalPage + 1);
-    const paginateModalBack = () => setCurrentModalPage(currentModalPage - 1);
-
-    return (
-        <div className="flex flex-col">
-            <div className="overflow-y-auto">
-                <div className='flex justify-between py-3 pl-2'>
-                    {/* <div className='relative max-w-xs'>
+  return (
+    <div className="flex flex-col">
+      <div className="overflow-y-auto">
+        <div className="flex justify-between py-3 pl-2">
+          {/* <div className='relative max-w-xs'>
                         <label htmlFor='service-search' className='sr-only'> Search </label>
                         <input
                             type="text"
@@ -51,42 +59,51 @@ export default function EgnyteService(){
                             />
                         </div>
                     </div> */}
-                    <div className='p-1.5 w-full inline-block align-middle'>
-                        <div className='overflow-hidden border rounded-md'>
-                            <table className='min-w-full divide-y table-auto divide-gray-200'>
-                                <thead className='bg-gray-50'>
-                                    <tr>
-                                        <th className='py-3 flex justify-center text-sm font-bold border-r text-gray-500'>Date</th>
-                                        <th className='px-[150px] text-sm font-bold text-left text-gray-500'>Name</th>
-                                    </tr>
-                                </thead>
-                                <tbody className='divide-y divide-gray-300'>
-                                   {egnyte.map(incident => {
-                                    return(
-                                        <tr key={incident.date}>
-                                            <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>{incident.date}</td>
-                                            <td className='px-4 py-4 text-sm text-gray-500 whitespace-nowrap'>{incident.name}</td>
-                                        </tr>
-                                    )
-                                   })}
-                                </tbody>
-                            </table>
-                            <ModalPagination
-                                dataPerPage={dataPerPage}
-                                totalData={egnyte.length}
-                                paginateModalBack={paginateModalBack}
-                                paginateModalFront={paginateModalFront}
-                                currentData={currentData}
-                            />
-                        </div>
-                    </div>
-                </div>
+          <div className="p-1.5 w-full inline-block align-middle">
+            <div className="overflow-hidden border rounded-md">
+              <table className="min-w-full divide-y table-auto divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="py-3 flex justify-center text-sm font-bold border-r text-gray-500">
+                      Date
+                    </th>
+                    <th className="px-[150px] text-sm font-bold text-left text-gray-500">
+                      Name
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-300">
+                  {egnyte ? (
+                    paginatedData.map((incident) => {
+                      return (
+                        <tr key={incident.date} className='odd:bg-gray-200'>
+                          <td className="px-4 py-3 text-sm text-gray-500 whitespace-nowrap">
+                            {incident.date}
+                          </td>
+                          <td className="px-4 py-3 text-sm text-gray-500 whitespace-nowrap">
+                            {incident.name}
+                          </td>
+                        </tr>
+                      );
+                    })
+                  ) : (
+                    <p>no data</p>
+                  )}
+                </tbody>
+              </table>
+              <ModalPagination
+                dataPerPage={dataPerPage}
+                totalData={egnyte.length}
+                currentModalPage={currentModalPage}
+                // currentData={1}
+                paginate={paginate}
+              />
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-
-// todo: apply filter if applicable
-// todo: apply search if applicable
-// todo: optimize view port to show recent pulls first
+// todo: apply search 
