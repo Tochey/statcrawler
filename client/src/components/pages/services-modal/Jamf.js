970fe724dcdfc1533a333b273e773fdaf63d2930
David Ogar <owogogahhero@outlook.com>
2022-10-08 07:00:30 +0100

    fix: merge changes

--- client/src/components/pages/services-modal/Jamf.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function JamfData(){
-    return(
-        <div></div>
-    )
-}
--- client/src/components/pages/services-modal/Zoom.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ZoomData(){
-    return(
-        <div></div>
-    )
-}
